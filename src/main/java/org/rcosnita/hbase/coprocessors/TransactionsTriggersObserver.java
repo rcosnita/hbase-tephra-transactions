fff5be93824fc9bc6192ff4506a0656709246525
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2018-09-17 12:17:53 +0300

    Add an initial example of tephra transactional system.

--- src/main/java/org/rcosnita/hbase/coprocessors/TransactionsTriggersObserver.java
+++ src/main/java/org/rcosnita/hbase/coprocessors/TransactionsTriggersObserver.java
@@ -1,13 +1,23 @@
 package org.rcosnita.hbase.coprocessors;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.client.Durability;
-import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.CoprocessorEnvironment;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.tephra.TransactionContext;
+import org.apache.tephra.TransactionFailureException;
+import org.apache.tephra.TransactionSystemClient;
+import org.apache.tephra.hbase.TransactionAwareHTable;
+import org.apache.tephra.runtime.*;
 import org.rcosnita.hbase.proto.Transactions;
 
 import java.io.IOException;
@@ -16,19 +26,69 @@ import java.io.IOException;
  * TransactionsTriggersObserver provides the logic for executing all the operations encoded in the given request.
  */
 public class TransactionsTriggersObserver extends BaseRegionObserver {
+    private final static byte[] COLUMN_FAMILY_TRANS = Bytes.toBytes("trans");
+    private final static byte[] CELL_NAME_ACC_TRANS = Bytes.toBytes("acc1");
+
     private final static byte[] COLUMN_FAMILY = Bytes.toBytes("trans");
     private final static byte[] CELL_NAME = Bytes.toBytes("tran");
+
+    private Connection connection;
+    private TransactionSystemClient tephraClient;
+
+    public TransactionsTriggersObserver() throws IOException {
+        super();
+
+        Configuration conf = HBaseConfiguration.create();
+        connection = ConnectionFactory.createConnection(conf);
+
+        Injector injector = Guice.createInjector(
+                new ConfigModule(conf),
+                new ZKModule(),
+                new DiscoveryModules().getDistributedModules(),
+                new TransactionModules().getDistributedModules(),
+                new TransactionClientModule()
+        );
+
+        tephraClient = injector.getInstance(TransactionSystemClient.class);
+    }
+
+    private TransactionAwareHTable getTransTable() throws IOException {
+        Table table = connection.getTable(TableName.valueOf("transactions"));
+        TransactionAwareHTable transTable = new TransactionAwareHTable(table);
+        return transTable;
+    }
 
     @Override
     public void prePut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, Durability durability) throws IOException {
         Cell c = put.get(COLUMN_FAMILY, CELL_NAME).get(0);
         byte[] tranBytes = new byte[c.getValueLength()];
         System.arraycopy(c.getValueArray(), c.getValueOffset(), tranBytes, 0, c.getValueLength());
-
         Transactions.Transaction tran = Transactions.Transaction.parseFrom(tranBytes);
-        put.getFamilyCellMap().clear();
-        put.addColumn(COLUMN_FAMILY, Bytes.toBytes("account"), Bytes.toBytes(tran.getAcc1()));
-        put.addColumn(COLUMN_FAMILY, Bytes.toBytes("value"), Bytes.toBytes(tran.getValue1()));
+
+        try (TransactionAwareHTable transTable = getTransTable()) {
+            TransactionContext context = new TransactionContext(tephraClient, transTable);
+            try {
+                context.start();
+
+                Put putTran = new Put(Bytes.toBytes("transaction1-glob"));
+                putTran.addColumn(COLUMN_FAMILY_TRANS, CELL_NAME_ACC_TRANS, Bytes.toBytes(tran.getValue1()));
+                transTable.put(putTran);
+
+                putTran = new Put(Bytes.toBytes("transaction2-glob"));
+                putTran.addColumn(COLUMN_FAMILY_TRANS, CELL_NAME_ACC_TRANS, Bytes.toBytes(-tran.getValue1()));
+                transTable.put(putTran);
+
+                context.finish();
+            } catch (Exception ex) {
+                try {
+                    context.abort();
+                } catch (TransactionFailureException tfex) {
+                    System.err.println(tfex);
+                    e.complete();
+                    return;
+                }
+            }
+        }
 
         super.prePut(e, put, edit, durability);
     }
--- src/test/java/org/rcosnita/hbase/coprocessors/TransactionsTriggersObserverTests.java
+++ src/test/java/org/rcosnita/hbase/coprocessors/TransactionsTriggersObserverTests.java
@@ -6,14 +6,18 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.rcosnita.hbase.proto.Transactions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class TransactionsTriggersObserverTests {
-    private Table transTable;
+    volatile Connection connection;
 
     @Before
     public void setup() throws Exception {
@@ -24,19 +28,62 @@ public class TransactionsTriggersObserverTests {
                 .getPath();
         conf.addResource(new Path(path));
         HBaseAdmin.checkHBaseAvailable(conf);
-        Connection connection = ConnectionFactory.createConnection(conf);
-        transTable = connection.getTable(TableName.valueOf("transactions_triggers"));
+        connection = ConnectionFactory.createConnection(conf);
     }
 
     @Test
-    public void testPutTransactionDecodingWorks() throws IOException {
-        Transactions.Transaction tran = Transactions.Transaction.newBuilder()
-                .setAcc1("works as expected")
-                .setValue1(150)
-                .build();
+    public void testPutTransactionTephraEnabled() throws InterruptedException, IOException, ExecutionException {
+        final int ITERATIONS = 40;
+        testWriteWithExecutorService(ITERATIONS);
 
-        Put put = new Put(Bytes.toBytes("tran1"));
-        put.addColumn(Bytes.toBytes("trans"), Bytes.toBytes("tran"), tran.toByteArray());
-        transTable.put(put);
+        Get get = new Get(Bytes.toBytes("transaction1-glob"));
+        Result result = getTransDetailsTable().get(get);
+        Assert.assertNotNull(result);
+
+        byte[] tranBytes = result.getValue(Bytes.toBytes("trans"), Bytes.toBytes("acc1"));
+        long value = Bytes.toLong(tranBytes);
+        System.out.println(value);
+    }
+
+    private Table getTransTable() throws IOException {
+        return connection.getTable(TableName.valueOf("transactions_triggers"));
+    }
+
+    private Table getTransDetailsTable() throws IOException {
+        return connection.getTable(TableName.valueOf("transactions"));
+    }
+
+    private void testWriteWithExecutorService(int numOfIterations) throws InterruptedException, ExecutionException {
+        ExecutorService executor = new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 2);
+        List<Future<?>> pendingFutures = new ArrayList<Future<?>>();
+
+        for (int i = 0; i < numOfIterations; i++) {
+             writeRandomTransaction(i + 1, executor, pendingFutures);
+        }
+
+        for (Future<?> f : pendingFutures) {
+            f.get();
+        }
+    }
+
+    private void writeRandomTransaction(final int seed, ExecutorService executor, List<Future<?>> pendingFutures) {
+        pendingFutures.add(executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                Transactions.Transaction tran = Transactions.Transaction.newBuilder()
+                        .setAcc1("works as expected")
+                        .setValue1(150 * seed)
+                        .build();
+
+                Put put = new Put(Bytes.toBytes("tran:" + seed));
+                put.addColumn(Bytes.toBytes("trans"), Bytes.toBytes("tran"), tran.toByteArray());
+
+                try (Table table = getTransTable()) {
+                    table.put(put);
+                } catch (Exception ex) {
+                    System.err.println(ex);
+                }
+            }
+        }));
     }
 }
